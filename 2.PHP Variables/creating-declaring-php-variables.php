cbe94ad16b85e963a67403c6a62d940fc0728204
nuth-vireak <timberr.4t@gmail.com>
2023-02-08 22:48:30 +0700

    Initial commit

---
+++ 2.PHP Variables/creating-declaring-php-variables.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+    $txt = "Hello world!";
+    $x = 5;
+    $y = 10.5;
+
+    echo $txt;
+    echo "<br>";
+    echo $x;
+    echo "<br>";
+    echo $y;
+?>
+
+</body>
+</html>
